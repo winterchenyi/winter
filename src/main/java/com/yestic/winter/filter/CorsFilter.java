b417ac3b000a19c98f7757e1f77fa84872ddbfa1
chenyi <chenyi@hongguaninfo.com>
2017-12-25 11:10:33 +0800

    参数验证开发

--- src/main/java/com/yestic/winter/filter/CorsFilter.java
+++ src/main/java/com/yestic/winter/filter/CorsFilter.java
@@ -1,33 +1,33 @@
-//package com.yestic.winter.filter;
-//
-//import javax.servlet.*;
-//import javax.servlet.http.HttpServletResponse;
-//import java.io.IOException;
-//
-///**
-// * 支持跨域设置
-// * Created by chenyi on 2017/12/22
-// */
-//public class CorsFilter implements Filter {
-//
-//    @Override
-//    public void init(FilterConfig filterConfig) throws ServletException {
-//
-//    }
-//
-//    @Override
-//    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-//        HttpServletResponse response = (HttpServletResponse)servletResponse;
-//        response.setHeader("Access-Control-Allow-Origin", "*");
-//        response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE");
-//        response.setHeader("Access-Control-Max-Age", "3600");//设置过期时间
-//        response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, token");
-//        filterChain.doFilter(servletRequest, servletResponse);
-//    }
-//
-//    @Override
-//    public void destroy() {
-//
-//    }
-//
-//}
+package com.yestic.winter.filter;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * 支持跨域设置
+ * Created by chenyi on 2017/12/22
+ */
+public class CorsFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletResponse response = (HttpServletResponse)servletResponse;
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE");
+        response.setHeader("Access-Control-Max-Age", "3600");//设置过期时间
+        response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, token");
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+
+}
